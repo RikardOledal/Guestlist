46e9795a52657811e67c7cbda09122def1dd0f48
Rikard Oledal <rikard@rexcel.se>
2021-11-03 19:30:44 +0100

    Added application name and version

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 # Python Application
-print("Welcome to Rikard v1.01")
+application_name = "Friendster"
+version = "1.0"
+
+print("Welcome to the {0}. Version v{1}!".format(application_name, version))
