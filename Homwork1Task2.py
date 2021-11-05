2df6fd74d95832b4051009cab16aeaf754baee6f
Rikard Oledal <rikard@rexcel.se>
2021-11-05 10:44:08 +0100

    Text of Fresh Prince

---
+++ Homwork1Task2.py
@@ -0,0 +1,7 @@
+# Fresh prince of bel air
+
+Intro = "Now this is a story all about how"
+Bridge1 = "My life got flipped turned upside down"
+Bridge2 = "And I'd like to take a minute, just sit right there"
+Outro = "I'll tell you how I became the prince of a town called Bel-Air"
+print(Intro, Bridge1, Bridge2, Outro, sep='\n')
