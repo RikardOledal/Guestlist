922e935904341895fbbbc698af1dc603d7c8068b
Rikard Oledal <rikard@rexcel.se>
2021-11-05 15:34:53 +0100

    Added new row beetween numbers

--- Hashtagnumbers.py
+++ Hashtagnumbers.py
@@ -8,16 +8,16 @@ Lefttag = "#"+"\t"+" "+"\n"
 Midtag = "   "+"#"+"   "+"\n"
 
 # Creating numbers from the buildingblocks
-Zero = Hashtagline + TabTag*7 + Hashtagline
-One = Righttag*9
-Two = Hashtagline + Righttag*3 + Hashtagline + Lefttag*3 + Hashtagline
-Tree = Hashtagline + Righttag*3 + Hashtagline + Righttag*3 + Hashtagline
-Four = TabTag*3 + Hashtagline + Righttag*4
-Five = Hashtagline + Lefttag*3 + Hashtagline + Righttag*3 + Hashtagline
-Six = Hashtagline + Lefttag*3 + Hashtagline + TabTag*3 + Hashtagline
-Seven = Hashtagline + Righttag*8
-Eight = Hashtagline + TabTag*3 + Hashtagline + TabTag*3 + Hashtagline
-Nine = Hashtagline + TabTag*3 + Hashtagline + Righttag*3 + Hashtagline
+Zero = Hashtagline + TabTag*7 + Hashtagline + "\n"
+One = Righttag*9+ "\n"
+Two = Hashtagline + Righttag*3 + Hashtagline + Lefttag*3 + Hashtagline+ "\n"
+Tree = Hashtagline + Righttag*3 + Hashtagline + Righttag*3 + Hashtagline+ "\n"
+Four = TabTag*3 + Hashtagline + Righttag*4+ "\n"
+Five = Hashtagline + Lefttag*3 + Hashtagline + Righttag*3 + Hashtagline+ "\n"
+Six = Hashtagline + Lefttag*3 + Hashtagline + TabTag*3 + Hashtagline+ "\n"
+Seven = Hashtagline + Righttag*8+ "\n"
+Eight = Hashtagline + TabTag*3 + Hashtagline + TabTag*3 + Hashtagline+ "\n"
+Nine = Hashtagline + TabTag*3 + Hashtagline + Righttag*3 + Hashtagline+ "\n"
 
 # Printing the numbers from 0-9
 print(Zero+One+Two+Tree+Four+Five+Six+Seven+Eight+Nine)
