089177081e1f5fa3609b3db1c03557d422cfc480
Rikard Oledal <rikard@rexcel.se>
2021-11-08 12:01:21 +0100

    My homework for lesson 2

---
+++ Homework2.py
@@ -0,0 +1,25 @@
+# Task 1: The Greeting program
+
+name = "rikard"
+day = "monday"
+
+print("Good day " + name.title() + "! " + day.title() + " is a perfect day to learn some python.")
+
+# Task 2: My name
+
+firstname = "rikard"
+lastname = "oledal"
+
+print(firstname.title() + " " + lastname.title())
+
+# Task 3: The Calculator
+
+a = 7
+b = 4
+
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+print(a ** b)
+print(a // b)
