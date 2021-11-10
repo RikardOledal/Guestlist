fa8c854dfb30be8f8d92d2fd659d1dfc76fcb39f
Rikard Oledal <rikard@rexcel.se>
2021-11-10 14:01:06 +0100

    Homework for lesson 3

--- Homework3.py
+++ Homework3.py
@@ -9,23 +9,33 @@ else:
 
 # Task 2: Phonenumber-check
 
-phone = "085694A22"
+phone = "0738394420"
 i = 0
-roof = 10
+roof = len(phone)-1
 
-# Check that the length of the number is 10 digits
+# Check length
 if len(phone) != 10:
     print("Your number phonenumber must be 10 digits")
-else:
-    print("Your number is the correct length")
-# Check if any numbers are not digits
-while i <= 10:
+# Check digits
+while i <= roof:
     if not phone[i].isdigit():
-        print(f"The {i-1} character is not a digit")
-        i += 1
-        continue
+        print(f"The {i+1} character is not a digit")
+    i += 1
+    continue
+# Check digits and length
 if phone.isdigit and len(phone) == 10:
-    print("It is a correct phonenumber with 10 numbers")
+    print("It is a correct phonenumber with 10 digits")
 else:
-    print("Correct the Errors above!")
+    print("Correct the errors above!")
 
+# Task 3: Name-check
+
+my_name = "rikard"
+print("Guess what name I am thinking of")
+guess = input()
+if guess.isupper:
+    guess = guess.lower()
+if guess == my_name:
+    print("Congratulations!!! You guessed right")
+else:
+    print("You are wrong!!!")
