e007b1d24f01b2a52541b18032f20afc26a0c9d8
Rikard Oledal <rikard@rexcel.se>
2021-11-11 13:44:37 +0100

    update

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 # Python Application
-application_name = "Friendster"
-version = "1.0"
-
-print("Welcome to the {0}. Version v{1}!".format(application_name, version))
+import random
+print("Lets see your math-skilz!!!")
+sign = random.randint(1, 4)
+    # 1 == +
+    # 2 == -
+    # 3 == *
+    # 4 == /
+firstnumber = random.randint(1, 9)
+secondnumber = random.randint(1, 9)
+answer = input(f"What is {firstnumber} / {secondnumber}")
+if float(answer) == firstnumber / secondnumber:
+    print("That is correct! You are a genius!!!")
+else:
+    print(f"WRONG!!! The right answer is {firstnumber/secondnumber}")
