867e7cc59fccda7bbebfa32674e3beacdd69034f
Rikard Oledal <rikard@rexcel.se>
2021-11-13 13:03:16 +0100

    Updates

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-# Python Application
-import random
-print("Lets see your math-skilz!!!")
-sign = random.randint(1, 4)
-    # 1 == +
-    # 2 == -
-    # 3 == *
-    # 4 == /
-firstnumber = random.randint(1, 9)
-secondnumber = random.randint(1, 9)
-answer = input(f"What is {firstnumber} / {secondnumber}")
-if float(answer) == firstnumber / secondnumber:
-    print("That is correct! You are a genius!!!")
-else:
-    print(f"WRONG!!! The right answer is {firstnumber/secondnumber}")
+options = input("input opperator: ")
+firstnumber = int(input("input first number: "))
+secondnumber = int(input("input second number: "))
+if options == "+" :
+    result = firstnumber + secondnumber
+    print("answer:", result)
+elif options == "-":
+    result = firstnumber - secondnumber
+    print("answer:", result)
+elif options == "*":
+    result = firstnumber * secondnumber
+    print("answer:", result)
+elif options == "/":
+    result = firstnumber / secondnumber
+    print("answer:", result)
