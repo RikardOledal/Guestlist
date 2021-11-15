2b2db5f2f27d322f19e34e8cf8aa905e96b8b5cc
Rikard Oledal <rikard@rexcel.se>
2021-11-15 13:16:27 +0100

    Hernadez math-quiz - my version

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
-options = input("input opperator: ")
-firstnumber = int(input("input first number: "))
-secondnumber = int(input("input second number: "))
-if options == "+" :
+import random
+# display intro
+title = ' ** Math Quiz ** '
+print ('*' * len(title))
+print (title)
+print ('*' * len(title))
+# display menu
+menu_list = ('1. Addition', '2. Subtraction', '3. Multiplication', '4. Integer Division', '5. Exit')
+print (menu_list[0])
+print (menu_list[1])
+print (menu_list[2])
+print (menu_list[3])
+print (menu_list[4])
+# display_separator
+print ('-' * 24)
+# user input
+options = int(input('input opperator:'))
+while options > 5 or options < 1 :
+    print ('Invalid menu option.')
+    options = int(input('Please try again:'))
+# Operations
+firstnumber = random.randint(1,100)
+secondnumber = random.randint(1,100)
+if options == 1 :
+    print(f"What is {firstnumber} + {secondnumber}?")
     result = firstnumber + secondnumber
-    print("answer:", result)
-elif options == "-":
+elif options == 2:
+    print(f"What is {firstnumber} - {secondnumber}?")
     result = firstnumber - secondnumber
-    print("answer:", result)
-elif options == "*":
+elif options == 3:
+    print(f"What is {firstnumber} * {secondnumber}?")
     result = firstnumber * secondnumber
-    print("answer:", result)
-elif options == "/":
+elif options == 4:
+    print(f"What is {firstnumber} / {secondnumber}?")
     result = firstnumber / secondnumber
-    print("answer:", result)
+#user solution
+usersolution = int(input('input solution: '))
+if usersolution == result :
+    print ('Correct')
+else:
+    print ('Incorrect')
+    print("Correct answer:", result)
