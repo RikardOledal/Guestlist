9c63d1d4f68b2bd72a385299683bf1d0220d5c60
Rikard Oledal <rikard@rexcel.se>
2021-11-19 23:23:58 +0100

    update

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
-numbers = list(range(1,11))
-squared_tup = [(i, i*i) for i in numbers]
-print(squared_tup)
+def intro(task):
+    title = f" ** {task} ** "
+    print ("\n")
+    print ("*" * len(title))
+    print (title)
+    print ("*" * len(title))
+    print ("\n")
+
+def make_country(land):
+    print ("{} is the capital of {}".format(land["capital"] , land["name"]))
+
+land = input("Country:")
+capitol = input("Capitol:")
+
+dict_1 = {
+    "name": land,
+    "capital": capitol
+    "Population": 1000000
+}
+
+finland = {
+    "name": "Finland",
+    "capital": "Helsingfors"
+}
+
+make_country(dict_1)
