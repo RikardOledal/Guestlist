dd6ad050b3ace168673696d3ccd62c2a076f94a4
Rikard Oledal <rikard@rexcel.se>
2021-11-20 13:47:32 +0100

    Update

--- game.py
+++ game.py
@@ -17,6 +17,19 @@ print("Let´s start by creating your chacter")
 
 print("Here are your choises")
 race = ["Human", "Dwarf", "Elf"]
-for item in race:
-    print(item)
-print("FUNKA")
+
+def chooselist(alt):
+    print("@"*30)
+    print("@@ Your choises are         @@")
+    print("@@                          @@")
+    x = 0
+    for item in alt:
+        x += 1
+        print(f"@@ {x}. {item} "+" "*(22-len(item)-len(str(x)))+"@"*2)
+    print("@"*30)
+    choise = input("Make your choise! (just the number)")
+    return choise
+
+if chooselist(race):
+
+
