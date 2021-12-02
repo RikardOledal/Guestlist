bc500dcd9f8897465c1fc7a605af8b611efb45bb
Rikard Oledal <rikard@rexcel.se>
2021-12-02 15:07:07 +0100

    Update

--- Homework/Homework11.py
+++ Homework/Homework11.py
@@ -96,14 +96,48 @@ class Mathematician:
             raise ValueError("nums should be a list")
         return [n for n in nums if (n % 100 != 0 and n % 4 == 0) or (n % 100 == 0 and n % 400 == 0)]   
 
-    
-testlist1 = [1, 2, 3, 4]
-testlist2 = [-1, 2, -3, 4]
-testlist3 = [1824, 1900, 2000, 1976, 1977, 1500, 1600]
 m = Mathematician()
-print(m.square_nums(testlist1))
-print(m.remove_positives(testlist2))
-print(m.filter_leaps(testlist3))
 
-
-
+while True:
+    print("1. Square numbers")
+    print("2. Remove positives")
+    print("3. Filter leaps")
+    print("4. Exit")
+    print("\n")
+    choise = input("What would you like to do? (1-4)")
+    try:
+        int(choise)
+    except ValueError:
+        print("Choise must be a number 1-3")
+    else:
+        choise = int(choise)
+    if choise == 4:
+        print("Ok. Bye!!!")
+        break
+    if choise < 1 or choise > 4:
+        print("Invalid choise")
+        continue
+    numbers = []
+    x = input("What number do you want to add?")
+    while x != "n":
+        try:
+            int(x)
+        except ValueError:
+            x = input("What number do you want to add? (n to stop adding)")
+            continue
+        else:
+            x = int(x)
+            numbers.append(x)
+            x = input("What number do you want to add? (n to stop adding)")
+    if choise == 1:
+        print(numbers)
+        print(m.square_nums(numbers))
+    elif choise == 2:
+        print(numbers)
+        print(m.remove_positives(numbers))
+    elif choise == 3:
+        print(numbers)
+        print(m.filter_leaps(numbers))
+    else:
+        print("Invalid choise")
+    
