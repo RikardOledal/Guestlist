ac09015b85f9463c5a92d8015967fc6d40a75382
Rikard Oledal <rikard@rexcel.se>
2021-12-18 13:30:28 +0100

    update

--- Projects/friendster/app.py
+++ Projects/friendster/app.py
@@ -1,6 +1,6 @@
 # Friendster Application
 application_name = "Friendster"
 version = "1.0"
-
+print("hello")
 if __name__ == "__main__":
     welcome_message = "Welcome to the {}. Version v{}!".format(application_name, version)
