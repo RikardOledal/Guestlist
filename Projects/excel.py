2b014e726481c4bc1792b0b77d0afaa1895b980b
Rikard Oledal <rikard@rexcel.se>
2022-01-12 15:39:46 +0100

    Pandas_vs_excel_project

--- Projects/excel.py
+++ Projects/excel.py
@@ -1,5 +1,19 @@
-import csv
-with open('estfil.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+import pandas as pd
+import glob
+
+location = "C:/Users/user/Python/Python-Rikard/Projects/excelfiler/*.xlsx"
+excel_files = glob.glob(location)
+
+pd.set_option("display.max_rows",183)
+
+
+print(excel_files)
+
+df1 = pd.DataFrame()
+
+for file in excel_files:
+    df2 = pd.read_excel(file, index_col=0)
+    df1 = pd.concat([df1, df2])
+
+df1.to_excel("C:/Users/user/Python/Python-Rikard/Projects/Temp/Jobbat2020.xlsx")
+print(df1)
